5a859ad9c46f00cbaf09b6edb6b44810ae4329e2
MateusEdu77 <mateusqueirozdq@gmail.com>
2023-04-06 16:45:26 -0300

    Funcoes

--- Analise_de_Notas/init.py
+++ Analise_de_Notas/init.py
@@ -1,2 +1,5 @@
-def verificacao():
-    print('a')
+def verificacao(nome, ano, sala, escola, estado, cidade):
+    import os
+    diretorio_aluno = f'Analise_de_Notas/alunos/{estado}/{cidade}/{escola}/{ano} ano/sala {sala}/{nome}'
+    if not os.path.exists(diretorio_aluno):
+        os.makedirs(diretorio_aluno)
